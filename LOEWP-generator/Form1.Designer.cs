610ce58e77376f22a96045dde6d6fb10f4cee899
Brian-MacDonald-TRG <brian.macdonald@techresearchgroup.com>
2018-01-29 15:37:46 -0500

    Add project files.

---
+++ LOEWP-generator/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace LOEWP_generator
+{
+    partial class frmLOEWP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectTM = new System.Windows.Forms.Button();
+            this.lblSelectedXmlTM = new System.Windows.Forms.Label();
+            this.rtbOutputPreview = new System.Windows.Forms.RichTextBox();
+            this.btnExportCode = new System.Windows.Forms.Button();
+            this.lblOutputPreview = new System.Windows.Forms.Label();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSelectPDF = new System.Windows.Forms.Button();
+            this.lblSelectedPdfTM = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectTM
+            // 
+            this.btnSelectTM.Location = new System.Drawing.Point(28, 33);
+            this.btnSelectTM.Name = "btnSelectTM";
+            this.btnSelectTM.Size = new System.Drawing.Size(239, 40);
+            this.btnSelectTM.TabIndex = 0;
+            this.btnSelectTM.Text = "Open TM source code (ExampleTM.xml)";
+            this.btnSelectTM.UseVisualStyleBackColor = true;
+            this.btnSelectTM.Click += new System.EventHandler(this.btnSelectTM_Click);
+            // 
+            // lblSelectedXmlTM
+            // 
+            this.lblSelectedXmlTM.AutoSize = true;
+            this.lblSelectedXmlTM.Location = new System.Drawing.Point(521, 40);
+            this.lblSelectedXmlTM.Name = "lblSelectedXmlTM";
+            this.lblSelectedXmlTM.Size = new System.Drawing.Size(127, 13);
+            this.lblSelectedXmlTM.TabIndex = 1;
+            this.lblSelectedXmlTM.Text = "XML TM:   none selected";
+            // 
+            // rtbOutputPreview
+            // 
+            this.rtbOutputPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutputPreview.Location = new System.Drawing.Point(32, 117);
+            this.rtbOutputPreview.Name = "rtbOutputPreview";
+            this.rtbOutputPreview.Size = new System.Drawing.Size(833, 309);
+            this.rtbOutputPreview.TabIndex = 2;
+            this.rtbOutputPreview.Text = "";
+            // 
+            // btnExportCode
+            // 
+            this.btnExportCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExportCode.Enabled = false;
+            this.btnExportCode.Location = new System.Drawing.Point(744, 432);
+            this.btnExportCode.Name = "btnExportCode";
+            this.btnExportCode.Size = new System.Drawing.Size(121, 40);
+            this.btnExportCode.TabIndex = 3;
+            this.btnExportCode.Text = "&Export Code";
+            this.btnExportCode.UseVisualStyleBackColor = true;
+            // 
+            // lblOutputPreview
+            // 
+            this.lblOutputPreview.AutoSize = true;
+            this.lblOutputPreview.Location = new System.Drawing.Point(29, 101);
+            this.lblOutputPreview.Name = "lblOutputPreview";
+            this.lblOutputPreview.Size = new System.Drawing.Size(157, 13);
+            this.lblOutputPreview.TabIndex = 4;
+            this.lblOutputPreview.Text = "LOEPWP Code output preview:";
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(617, 432);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(121, 40);
+            this.btnCopyToClipboard.TabIndex = 5;
+            this.btnCopyToClipboard.Text = "&Copy to Clipboard";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(32, 432);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 6;
+            // 
+            // btnSelectPDF
+            // 
+            this.btnSelectPDF.Enabled = false;
+            this.btnSelectPDF.Location = new System.Drawing.Point(273, 33);
+            this.btnSelectPDF.Name = "btnSelectPDF";
+            this.btnSelectPDF.Size = new System.Drawing.Size(239, 40);
+            this.btnSelectPDF.TabIndex = 7;
+            this.btnSelectPDF.Text = "Open published TM (ExampleTM.pdf)";
+            this.btnSelectPDF.UseVisualStyleBackColor = true;
+            this.btnSelectPDF.Click += new System.EventHandler(this.btnSelectPDF_Click);
+            // 
+            // lblSelectedPdfTM
+            // 
+            this.lblSelectedPdfTM.AutoSize = true;
+            this.lblSelectedPdfTM.Location = new System.Drawing.Point(522, 54);
+            this.lblSelectedPdfTM.Name = "lblSelectedPdfTM";
+            this.lblSelectedPdfTM.Size = new System.Drawing.Size(126, 13);
+            this.lblSelectedPdfTM.TabIndex = 8;
+            this.lblSelectedPdfTM.Text = "PDF TM:   none selected";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(32, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 40);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "&Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.QuitApplication);
+            // 
+            // frmLOEWP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 504);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblSelectedPdfTM);
+            this.Controls.Add(this.btnSelectPDF);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCopyToClipboard);
+            this.Controls.Add(this.lblOutputPreview);
+            this.Controls.Add(this.btnExportCode);
+            this.Controls.Add(this.rtbOutputPreview);
+            this.Controls.Add(this.lblSelectedXmlTM);
+            this.Controls.Add(this.btnSelectTM);
+            this.Name = "frmLOEWP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TRG - List of Effective Pages Work Package Code Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectTM;
+        private System.Windows.Forms.Label lblSelectedXmlTM;
+        private System.Windows.Forms.RichTextBox rtbOutputPreview;
+        private System.Windows.Forms.Button btnExportCode;
+        private System.Windows.Forms.Label lblOutputPreview;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSelectPDF;
+        private System.Windows.Forms.Label lblSelectedPdfTM;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LOEWP-generator/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LOEWP_generator
+{
+    public partial class frmLOEWP : Form
+    {
+        OpenFileDialog fileSelected;
+        string lines;
+        string output;
+        string searchStringStart = "wpno=\"";
+        string searchStringEnd = "\"";
+        List<string> wpnoList = new List<string> { };
+
+        public frmLOEWP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectTM_Click(object sender, EventArgs e)
+        {
+            rtbOutputPreview.Clear();
+            //user browses to folder and selects file
+            fileSelected = new OpenFileDialog();
+            fileSelected.Filter = "XML Files (*.xml)|*.xml|Text Files (*.txt)|*.txt|SGML Files (*.sgml)|*.sgml";
+            fileSelected.Multiselect = false;
+
+            if (fileSelected.ShowDialog() == DialogResult.OK)
+            {
+                lines = File.ReadAllText(fileSelected.FileName);
+
+                lblSelectedXmlTM.Text = "XML TM:   " + fileSelected.SafeFileName;
+                //GenerateLOEWP();
+                btnSelectPDF.Enabled = true;
+            }
+        }
+
+        private void GenerateLOEWP()
+        {
+            if (lines != "" && lines != null)
+            {
+                char[] linesToChars = lines.ToCharArray();
+
+                // make a list of all positions of all of the tag attribute string [ wpno=" ] (w/o the square brackets)
+                List<int> positionList = GetPositions(lines, searchStringStart);
+                string[] wpNumbers = new string[positionList.Count];
+
+                foreach (int startPos in positionList)
+                {
+                    string thisWPNO = "";
+                    for (int i = 0; i < 100; i++)
+                    {
+                        if (linesToChars[searchStringStart.Length + startPos + i].ToString() != searchStringEnd)
+                        {
+                            thisWPNO += linesToChars[searchStringStart.Length + startPos + i];
+                        }
+                        else
+                        {
+                            wpnoList.Add(thisWPNO);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            output = "<loepwp>\r\n" +
+                "\t<title>List of Effective Pages/Work Packages</title>\r\n" +
+                "\t<note>\r\n" +
+                "\t\t<trim.para>NOTE: Zero in the \"Change No.\" column indicates an original page or work package.</trim.para>\r\n" +
+                "\t</note>\r\n" +
+                "\t<issuechg>\r\n" +
+                "\t\t<trim.para> Date of issue for the original manual is:</trim.para>\r\n" +
+                "\t\t<issued>\r\n" +
+                "\t\t\t<chgno> Original Draft </chgno>\r\n" +
+                "\t\t\t<chgdate julian=\"0000000\"> XXXXX </chgdate>\r\n" +
+                "\t\t</issued>\r\n" +
+                "\t</issuechg>\r\n" +
+                "\t<totalnumberof>\r\n" +
+                "\t\t<text> THE TOTAL NUMBER OF PAGES FOR FRONT AND REAR  MATTER IS </text>\r\n" +
+                "\t\t<totnum.frnt-rear-pages> XX </totnum.frnt-rear-pages>\r\n" +
+                "\t\t<text> AND THE TOTAL NUMBER OF WORK PACKAGES IS </text>\r\n" +
+                "\t\t<totnum.wps>" + wpnoList.Count + "</totnum.wps>\r\n" +
+                "\t\t<text>, CONSISTING OF THE FOLLOWING:</text>\r\n" +
+                "\t</totalnumberof>\r\n" +
+                "\t<col.title> Page / WP No.</col.title>\r\n" +
+                "\t<col.title> Change No.</col.title>\r\n";
+
+            foreach (string wpnoItem in wpnoList)
+            {
+                output += "\t<chghistory modified=\"none\">\r\n" +
+                    "\t\t<wpno wpref=\"" + wpnoItem + "\"/>\r\n" +
+                    "\t\t<wppages>X Pages</wppages>\r\n" +
+                    "\t\t<chgno>0</chgno>\r\n" +
+                    "\t</chghistory>\r\n";
+            }
+
+            output += "</loepwp>\r\n";
+            rtbOutputPreview.Text = output;
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rtbOutputPreview.Text);
+            lblStatus.Text = "Preview text copied to clipboard.";
+        }
+
+        public List<int> GetPositions(string source, string searchString)
+        {
+            List<int> ret = new List<int>();
+            int len = searchString.Length;
+            int start = -len;
+            while (true)
+            {
+                start = source.IndexOf(searchString, start + len);
+                if (start == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    ret.Add(start);
+                }
+            }
+            return ret;
+        }
+
+        private void QuitApplication(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("You are about to exit the application.\n\nProceed?", "Exit Application?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnSelectPDF_Click(object sender, EventArgs e)
+        {
+            //user browses to folder and selects file
+            fileSelected = new OpenFileDialog();
+            fileSelected.Filter = "PDF Files (*.pdf)|*.pdf";
+            fileSelected.Multiselect = false;
+
+            if (fileSelected.ShowDialog() == DialogResult.OK)
+            {
+                GenerateLOEWP();
+            }
+        }
+    }
+}
